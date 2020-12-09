f6481bf6835ba3d5957f0797a9f94b8b3ed21f09
hryer <harryermawan@gmail.com>
2020-12-09 16:51:40 +0700

    fix: fix deployment can`t read resolve alias webpack

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,13 @@
 import React, { lazy } from 'react';
 import { Redirect } from 'react-router-dom';
-import lazyComponent from '@/components/lazyComponent';
+import lazyComponent from './components/lazyComponent';
 // import Pokemons from '@/pages/Pokemons';
 
 const Pokemons = lazy(() =>
-  import(/* webpackChunkName: "Pokemons" */ '@/pages/Pokemons')
+  import(/* webpackChunkName: "Pokemons" */ './pages/Pokemons')
 );
 const Details = lazy(() =>
-  import(/* webpackChunkName: "Details" */ '@/pages/Details')
+  import(/* webpackChunkName: "Details" */ './pages/Details')
 );
 const Inventory = lazy(() => import(/* webpackChunkName: "Inventory" */ '@/pages/Inventory'));
 
